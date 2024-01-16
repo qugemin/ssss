7fc2511ad80f02f7705a91f88545ed10323c7993
qugemin <20020116@outlook.com>
2024-01-16 14:06:41 +0800

    xiugai

---
+++ text/src/components/lian/student.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>学生姓名：{{name}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"student",
+    data(){
+        return {
+            name:"",
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
